107a6934e3e2997fd931aadfef7ebb2b7e43654a
Nabin Karki <naabin@outlook.com>
2019-11-17 12:00:22 +1100

    Added visits, updated owner with relationship. Closes #26

--- pet-clinic-data/src/main/java/com/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Owner.java
@@ -1,6 +1,15 @@
 package com.petclinic.model;
 
+
+import java.util.Set;
+
 public class Owner extends Person {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6116274458963665495L;
+	
+	private Set<Pet> pets; 
 
 	public Owner(String firstName, String lastName) {
 		super(firstName, lastName);
@@ -8,9 +17,15 @@ public class Owner extends Person {
 	
 	public Owner() {
 	}
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6116274458963665495L;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 
 }
