b073b37c0c7365acfc9e133333471ce1c37a126a
Nabin Karki <naabin@outlook.com>
2019-11-23 14:44:36 +1100

    Added visit map service and spring data service. Closes #39

---
+++ pet-clinic-data/src/main/java/com/petclinic/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,49 @@
+package com.petclinic.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.petclinic.model.Visit;
+import com.petclinic.repositories.VisitRepository;
+import com.petclinic.services.VisitService;
+
+public class VisitSDJpaService implements VisitService {
+	
+	private final VisitRepository visitRepository;
+	
+	public VisitSDJpaService(VisitRepository visitRepository) {
+		this.visitRepository = visitRepository;
+	}
+
+	@Override
+	public Set<Visit> findAll() {
+		Set<Visit> visits = new HashSet<Visit>();
+		this.visitRepository.findAll().forEach(visits::add);
+		return visits;
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return this.visitRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Visit save(Visit object) {
+		if(object != null) {
+			return this.visitRepository.save(object);
+		}
+		return null;
+	}
+
+	@Override
+	public void delete(Visit object) {
+		this.visitRepository.delete(object);
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		this.visitRepository.findById(id);
+	}
+
+}
